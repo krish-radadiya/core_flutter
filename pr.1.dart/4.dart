6531440c8125d6ac09f67b99b8f4990dcdb476af
krish-radadiya <113992828+krish-radadiya@users.noreply.github.com>
2023-08-20 11:25:52 +0530

    Update 4.dart

--- pr.1.dart/4.dart
+++ pr.1.dart/4.dart
@@ -4,12 +4,12 @@ void main()
  List b=[[1,2,3],[4,6,8],[5,2,1]];
  List sum=[[5,7,4],[4,6,8],[9,2,9]];
  
- for(int i=0;i<3;i++)
+ for(int i=0;i<3;i++){
 
- for(int j=0;j<3;j++)
+ for(int j=0;j<3;j++){
  
  sum[i][j]=a[i][j]+b[i][j];
- 
+ }}
  print(sum);
  
 }
