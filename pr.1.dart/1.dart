a9099a36782d54dea27b950918c919756a3c9281
krish-radadiya <113992828+krish-radadiya@users.noreply.github.com>
2023-08-20 12:08:34 +0530

    Update 1.dart

--- pr.1.dart/1.dart
+++ pr.1.dart/1.dart
@@ -1,12 +1,23 @@
-void main()
-{
- List array=[0,-1,2,-5,4,-6,6,7];
- for(int i=0;i<array.length;i++)
- {
- if(array[i]<0) 
- {
- print("Negative Number are in array is");
- print(array[i]);
- }
- }
+import 'dart:io';
+
+void main() {
+  List<int> allNumbers = [];
+
+  print("Enter numbers: ");
+  String input;
+
+  do {
+    input = stdin.readLineSync()!;
+    if (input != 'a') {
+      int number = int.parse(input);
+      allNumbers.add(number);
+    }
+  } while (input != 'a');
+
+  print('\nNegative Elements in the Array:');
+  for (int i = 0; i < allNumbers.length; i++) {
+    if (allNumbers[i] < 0) {
+      print(allNumbers[i]);
+    }
+  }
 }
