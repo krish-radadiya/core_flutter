bea8b5ad00e919bd7fbb05904fedb11887417d59
krish-radadiya <113992828+krish-radadiya@users.noreply.github.com>
2023-08-20 12:19:05 +0530

    Update 3.dart

--- pr.1.dart/3.dart
+++ pr.1.dart/3.dart
@@ -1,15 +1,67 @@
-void main() {
-  List list1 = [1, 2, 3, 4, 6, 5, 7, 9, 7, 8, 5];
-  print("List is $list1");
+import 'dart:io';
 
-  list1.add(31);
-  print("New Added List is $list1");
+void main () {
+    List<String> product = ['phone', 'watch', 'airpods', 'leptop'];
 
-  list1[5] = 50;
-  print("Updated List is $list1");
+    while (true) {
+        print("$product\n");
+        print("Menu:");
+        print("1. Add product");
+        print("2. Update product");
+        print("3. Delete product");
+        print("4. View product");
+        print("5. Exit");
 
-  print("Delete 0 in the List : ");
-  list1.remove(0);
+        stdout.write("Enter your choice: ");
+        int choice = int.parse(stdin.readLineSync()!);
+        print("\n");
 
-  print(list1);
+        switch (choice) {
+        case 1:
+            stdout.write("Enter the product to add: ");
+            String newproduct = stdin.readLineSync()!;
+            product
+            .add(newproduct);
+            print("$newproduct added.");
+            break;
+
+        case 2:
+            stdout.write("Enter the index of the product to update: ");
+            int index = int.parse(stdin.readLineSync()!);
+            if (index >= 0 && index < product.length) {
+                stdout.write("Enter the updated product: ");
+                String updatedproduct = stdin.readLineSync()!;
+                product[index] = updatedproduct;
+                print("product updated.");
+            } else {
+                print("Invalid index.");
+            }
+            break;
+
+        case 3:
+            stdout.write("Enter the index of the product to delete: ");
+            int deleteIndex = int.parse(stdin.readLineSync()!);
+            if (deleteIndex >= 0 && deleteIndex < product.length) {
+                String deletedproduct = product.removeAt(deleteIndex);
+                print("$deletedproduct deleted.");
+            } else {
+                print("Invalid index.");
+            }
+            break;
+
+        case 4:
+            print("product List:");
+            for (int i = 0; i < product.length; i++) {
+                print("product $i: ${product[i]}");
+            }
+            break;
+
+        case 5:
+            print("Exiting the program.");
+            return;
+
+        default:
+            print("Invalid choice. Please select a valid option.");
+        }
+    }
 }
